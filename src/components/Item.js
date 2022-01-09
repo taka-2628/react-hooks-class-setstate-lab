b31a2cabf4f7a7a53c9e7d91e6792b3d00bcc94e
Takashi <th2826@columbia.edu>
2022-01-09 17:31:54 -0500

    Completed assignment

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,5 +1,31 @@
-import React, { useState } from "react";
+import React from "react";
 
+class Item extends React.Component{
+  state = {
+    isInCart: false
+  }
+
+  handleAddToCartClick() {
+    this.setState({isInCart : !this.state.isInCart});
+  }
+
+  render(){
+    return (
+      <li className={this.state.isInCart ? "in-cart" : ""}>
+        <span>{this.props.name}</span>
+        <span className="category">{this.props.category}</span>
+        <button
+          className={this.state.isInCart ? "remove" : "add"}
+          onClick={() => this.handleAddToCartClick()}
+        >
+          {this.state.isInCart ? "Remove From" : "Add to"} Cart
+        </button>
+      </li>
+    )
+  }
+}
+
+/*
 function Item({ name, category }) {
   const [isInCart, setIsInCart] = useState(false);
 
@@ -7,18 +33,8 @@ function Item({ name, category }) {
     setIsInCart((isInCart) => !isInCart);
   }
 
-  return (
-    <li className={isInCart ? "in-cart" : ""}>
-      <span>{name}</span>
-      <span className="category">{category}</span>
-      <button
-        className={isInCart ? "remove" : "add"}
-        onClick={handleAddToCartClick}
-      >
-        {isInCart ? "Remove From" : "Add to"} Cart
-      </button>
-    </li>
+  
   );
 }
-
+*/
 export default Item;
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,38 +1,43 @@
-import React, { useState } from "react";
+import React from "react";
 import Item from "./Item";
 
-function ShoppingList({ items }) {
-  const [selectedCategory, setSelectedCategory] = useState("All");
-
-  function handleCategoryChange(event) {
-    // event.target.value will be the value selected by the user
-    setSelectedCategory(event.target.value);
+class ShoppingList extends React.Component {
+  state = {
+    selectedCategory: "All"
   }
+  
+  handleCategoryChange(event) {
+    // event.target.value will be the value selected by the user
+    console.log(event.target.value)
+    this.setState({selectedCategory: event.target.value});
+  }
+  
+  render(){
+    // we want to filter the items to only display the ones based on the selected category
+    const itemsToDisplay = this.props.items.filter((item) => {
+      if (this.state.selectedCategory === "All") return true;
 
-  // we want to filter the items to only display the ones based on the selected category
-  const itemsToDisplay = items.filter((item) => {
-    if (selectedCategory === "All") return true;
+      return item.category === this.state.selectedCategory;
+    });
 
-    return item.category === selectedCategory;
-  });
-
-  return (
-    <div className="ShoppingList">
-      <div className="Filter">
-        <select name="filter" onChange={handleCategoryChange}>
-          <option value="All">Filter by category</option>
-          <option value="Produce">Produce</option>
-          <option value="Dairy">Dairy</option>
-          <option value="Dessert">Dessert</option>
-        </select>
+    return (
+      <div className="ShoppingList">
+        <div className="Filter">
+          <select name="filter" onChange={(event) => this.handleCategoryChange(event)}>
+            <option value="All">Filter by category</option>
+            <option value="Produce">Produce</option>
+            <option value="Dairy">Dairy</option>
+            <option value="Dessert">Dessert</option>
+          </select>
+        </div>
+        <ul className="Items">
+          {itemsToDisplay.map((item) => (
+            <Item key={item.id} name={item.name} category={item.category} />
+          ))}
+        </ul>
       </div>
-      <ul className="Items">
-        {itemsToDisplay.map((item) => (
-          <Item key={item.id} name={item.name} category={item.category} />
-        ))}
-      </ul>
-    </div>
-  );
+    )
+  }
 }
 
 export default ShoppingList;
